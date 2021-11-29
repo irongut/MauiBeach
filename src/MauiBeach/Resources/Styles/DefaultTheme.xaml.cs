e085dbc5af62ae6dfa7b847cf17bb5db9402fa90
irongut <murray.dave@outlook.com>
2021-11-29 21:55:56 +0000

    added default theme

---
+++ src/MauiBeach/Resources/Styles/DefaultTheme.xaml.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using Microsoft.Maui.Controls;
+using Microsoft.Maui.Essentials;
+
+namespace MauiBeach.Resources.Styles
+{
+    public partial class DefaultTheme : ResourceDictionary
+    {
+        public DefaultTheme()
+        {
+            InitializeComponent();
+        }
+    }
+}
