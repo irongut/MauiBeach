9c17f3c9304a1f65c8abd2282a941fad6c559e92
irongut <murray.dave@outlook.com>
2021-12-28 20:22:49 +0000

    added app + device info to About page

---
+++ src/MauiBeach/ViewModels/AboutViewModel.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.Maui.Essentials;
+using System;
+using System.Text;
+
+namespace MauiBeach.ViewModels;
+
+internal class AboutViewModel
+{
+    public string AppVersion => $"Version: {AppInfo.VersionString}{Environment.NewLine}Copyright © 2021 Taranis Software";
+
+    public string Platform
+    {
+        get
+        {
+            StringBuilder platform = new();
+            platform.Append("Platform: ").Append(DeviceInfo.Platform).Append(' ').AppendLine(DeviceInfo.VersionString);
+            platform.Append("Manufacturer: ").AppendLine(DeviceInfo.Manufacturer);
+            platform.Append("Device: ").AppendLine(DeviceInfo.Model);
+
+            return platform.ToString();
+        }
+    }
+}
