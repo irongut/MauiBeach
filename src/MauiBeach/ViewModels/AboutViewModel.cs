d6d247e0f739aa63a9dfc2e4be36f70543480e77
irongut <murray.dave@outlook.com>
2021-12-29 02:47:17 +0000

    added blog + github buttons to about

--- src/MauiBeach/ViewModels/AboutViewModel.cs
+++ src/MauiBeach/ViewModels/AboutViewModel.cs
@@ -1,13 +1,21 @@
 ﻿using MauiBeach.Services;
+using Microsoft.Maui.Controls;
 using Microsoft.Maui.Essentials;
 using System;
 using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Input;
 
 namespace MauiBeach.ViewModels;
 
 internal class AboutViewModel
 {
+    public ICommand BlogCommand { get; }
+    public ICommand GitHubCommand { get; }
+
     public string AppVersion => $"Version: {AppInfo.VersionString}{Environment.NewLine}Copyright © 2021 Taranis Software";
+
+    public string Description => "A playground for experiments with .Net MAUI. All code is available on GitHub and development is documented on my blog 'Sailing the Sharp Sea'.";
 
     public string Platform
     {
@@ -19,6 +27,36 @@ internal class AboutViewModel
             platform.Append("Device: ").AppendLine(DeviceInfoService.Model());
 
             return platform.ToString();
+        }
+    }
+
+    public AboutViewModel()
+    {
+        BlogCommand = new Command(async () => await OpenBlogAsync().ConfigureAwait(false));
+        GitHubCommand = new Command(async () => await OpenGitHubAsync().ConfigureAwait(false));
+    }
+
+    private async Task OpenBlogAsync()
+    {
+        try
+        {
+            await Browser.OpenAsync(new Uri("https://blog.taranissoftware.com/"), BrowserLaunchMode.External).ConfigureAwait(false);
+        }
+        catch (Exception)
+        {
+            throw;
+        }
+    }
+
+    private async Task OpenGitHubAsync()
+    {
+        try
+        {
+            await Browser.OpenAsync(new Uri("https://github.com/irongut/MauiBeach"), BrowserLaunchMode.External).ConfigureAwait(false);
+        }
+        catch (Exception)
+        {
+            throw;
         }
     }
 }
