c954fbde477ed47bb6458177fa97361bcf0645d4
irongut <murray.dave@outlook.com>
2021-12-31 23:19:58 +0000

    alert test

---
+++ src/MauiBeach/ViewModels/HomeViewModel.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Maui.Controls;
+using System.Windows.Input;
+
+namespace MauiBeach.ViewModels;
+
+internal class HomeViewModel
+{
+    public ICommand AlertCommand { get; }
+
+    public HomeViewModel()
+    {
+        AlertCommand = new Command(async () => await Shell.Current.DisplayAlert("Alert", "It's alerting!", "OK").ConfigureAwait(false));
+    }
+}
