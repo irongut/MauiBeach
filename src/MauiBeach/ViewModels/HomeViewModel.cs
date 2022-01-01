6fd1233ba0f3f7f466cc13a792c52ada9e38b9a3
irongut <murray.dave@outlook.com>
2022-01-01 00:20:14 +0000

    added action sheet + prompt tests

--- src/MauiBeach/ViewModels/HomeViewModel.cs
+++ src/MauiBeach/ViewModels/HomeViewModel.cs
@@ -1,4 +1,5 @@
 ﻿using Microsoft.Maui.Controls;
+using System.Diagnostics;
 using System.Windows.Input;
 
 namespace MauiBeach.ViewModels;
@@ -6,9 +7,21 @@ namespace MauiBeach.ViewModels;
 internal class HomeViewModel
 {
     public ICommand AlertCommand { get; }
+    public ICommand ActionSheetCommand { get; }
+    public ICommand PromptCommand { get; }
 
     public HomeViewModel()
     {
         AlertCommand = new Command(async () => await Shell.Current.DisplayAlert("Alert", "It's alerting!", "OK").ConfigureAwait(false));
+        ActionSheetCommand = new Command(async () =>
+        {
+            string action = await Shell.Current.DisplayActionSheet("ActionSheet Demo", "Cancel", "View", "Rename", "Delete").ConfigureAwait(false);
+            Debug.WriteLine($"ActionSheet Demo: {action}");
+        });
+        PromptCommand = new Command(async () =>
+        {
+            string prompt = await Shell.Current.DisplayPromptAsync("Question", "What is your name?").ConfigureAwait(false);
+            Debug.WriteLine($"Prompt Demo: {prompt}");
+        });
     }
 }
